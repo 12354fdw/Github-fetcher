87503f2336cdb062d9f94b532fa87e121750e11a
12354fdw <88530369+12354fdw@users.noreply.github.com>
2024-07-10 17:53:19 +0700

    Update Gitfetch.lua

--- Gitfetch.lua
+++ Gitfetch.lua
@@ -7,21 +7,56 @@ local out = args[4] or ""
 local thing = user.."/"..repo.."/"..branch
 local link = "https://raw.githubusercontent.com/"..thing.."/"
 
-local s = shell.run("wget",link.."manifest.lua","manifest_"..thing)
+function get(data)
 
-if not s then
-    print("repo does not exists or the repo does not have manifest")
+    local name = data[1]
+    local ok, e = http.checkURL(link..name)
+    if not ok then
+        print("FAILURE [FILE "..name.." does not exists in the repo!")
+        return "FAILURE"
+    end
+
+    print("Getting "..name)
+    if fs.exists(out.."/"..name) then
+        print("WARNING [FILE "..out.."/"..name.." ALREADY EXISTS] OVERWRITING")
+    end
+
+    local response = http.get(link..name)
+    if not response then
+        print("FAILED [HTTP ERROR]")
+        return "FAILURE"
+    end
+    local readed = response.readAll()
+    response.close()
+    
+    local dir = out.."/"..name
+
+    if data[2] == true then
+        dir = data[3]
+    end
+
+    local write = fs.open(dir,"wb")
+    write.write(readed)
+    write.close()
+end
+
+local what = get("manifest.lua")
+if what == "FAILURE" then
     return
 end
 
-local manifest = require("manifest_"..thing)
+local manifest = require(out.."/".."manifest")
+
+print()
 
 for i,v in pairs(manifest) do
-    print("Getting "..v)
-    shell.run("wget",link..v,out.."/"..v)
+    local what = get(v)
+    if what == "FAILURE" then
+        return
+    end
 end
 
-shell.run("delete","manifest_"..thing)
+fs.delete(out.."/manifest.lua")
 
 print()
 print("download complete")
