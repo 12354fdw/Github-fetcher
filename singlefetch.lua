7f010f997d13037eda09f92234e3934d4269f74e
12354fdw <88530369+12354fdw@users.noreply.github.com>
2024-07-24 19:19:08 +0700

    Create singlefetch.lua

---
+++ singlefetch.lua
@@ -0,0 +1,50 @@
+local args = {...}
+local user = args[1]
+local repo = args[2]
+local branch = args[3]
+local file = args[4]
+
+local programName = args[0] or fs.getName(shell.getRunningProgram())
+
+local dir = fs.getDir()
+
+thing = user.."/"..repo.."/"..branch
+link = "https://raw.githubusercontent.com/"..thing.."/"
+
+function get(data)
+
+    local name = data[1]
+    local ok, e = http.checkURL(link..name)
+    if not ok then
+        print("FAILURE [FILE "..name.." does not exists in the repo!")
+        return "FAILURE"
+    end
+
+    print("Getting "..name)
+    if fs.exists(out.."/"..name) then
+        print("WARNING [FILE "..out.."/"..name.." ALREADY EXISTS] OVERWRITING")
+    end
+
+    local response = http.get(link..name)
+    if not response then
+        print("FAILED [HTTP ERROR]")
+        print("Usage: "..programName.." <user> <repo> <branch> <file> [output dir]")
+        return "FAILURE"
+    end
+    local readed = response.readAll()
+    response.close()
+    
+    local dir = out.."/"..name
+
+    if data[2] == true then
+        dir = data[3]
+    end
+
+    local write = fs.open(dir,"wb")
+    write.write(readed)
+    write.close()
+    table.insert(downloaded,dir)
+end
+
+local data = {link..file,false}
+get(data)
